d4fd8942127d2e1875d2deb2bd268a2016b132f7
Henry Cabello <hacu95@hotmail.com>
2018-07-25 18:58:59 -0400

    another fix

--- deployments/qa.sh
+++ deployments/qa.sh
@@ -1,5 +1,5 @@
-npm install -g now
-echo "installing now"
-URL=$(now --docker -t $NOW_TOKEN)
+sudo npm install -g --unsafe-perm now
+echo "deploying..."
+URL=$(now --docker --public -t $NOW_TOKEN)
 echo "running acceptance on $URL"
-curl  --silent -L $URL
+curl --silent -L $URL
